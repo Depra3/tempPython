95d4d8a2de0974c84797b47e356193a38d4719ba
Depra3 <parkmin0318@gmail.com>
2022-12-27 10:48:10 +0900

    updated

---
+++ def221227/temp_03_basic.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 컨텍스트 매니저
+# 파티 식당 종업원
+# - 테이블 세팅
+# - 손님 식사 주문 / 식사
+# - 테이블 닦고 / 테이블 정리 / 다음 손님 맞을 준비
+
+# 활용 예제
+# 데이터를 모음 --> 업데이트 / DB 작업후 종료
+
+
+def main():
+    with open("data/my_file.txt") as my_file:
+        text = my_file.read()
+        text_length = len(text)
+    # with절 역할 : 파일을 열고 처리 완료 후 종료
+
+    print("파일 글자 총 길이는 {}과 같다.".format(text_length))
+
+if __name__ == "__main__":
+    main()
---
+++ def221227/temp_04_basic.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# context manager 작성
+import contextlib
+import sqlite3
+from contextlib import asyncontextmanager
+# context 매니저 패턴
+# open / close
+# Lock / Release
+# Change / Reset
+# Enter / Exit
+# Start / Stop
+# Setup / TearDown
+# Connect / Disconnect
+
+# 일반적인 형태
+###
+# 이 함수는 context manager를 사용하겠다 라고 명시
+@contextlib.contextmanager 
+# 동기화 처리
+def db_connect(url):
+
+    db = sqlite3.db_connect(url)
+    yield db
+
+    db.close()
+
+#비동기화 처리
+@asyncontextmanager
+def db_connect(url):
+
+    db = sqlite3.db_connect(url)
+    yield db
+
+    db.close()
+
+def main():
+    url = None
+    db_connect(url)
+###
